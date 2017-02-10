2155fccc80bf61cacdbf6b6b9de209712b9c39f3
AutisticFagetto420 <gm.terkeurs@student.alfa-college.nl>
2017-02-10 11:32:15 +0100

    bestandsnamen gewijzigd

---
+++ S2_squeeze.php
@@ -0,0 +1,25 @@
+<!-- ********************************************************************
+Het is moeilijk om deze webpagina er niet cheesy er uit te laten zien
+Mijn excuses
+******************************************************************** -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lotsamonny Inc.</title>
+	<link rel="stylesheet" type="text/css" href="S2_style.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+<div class="contentContainer">
+<h1 class="welkom">Welkom bij lotsamonny</h1>
+<p align="center">LotsaMonny is een bedrijf dat internetmarketing cursussen aanbied aan mensen als u!</p>
+<p align="center">We bieden 3 verschillende workshops aan, met daarin echt advies een een proffesional</p>
+<br><br>
+<h2 align="center">Schrijf u nu in!</h2>
+<input type="text" placeholder="E-mail"></input>
+<br>
+<input type="submit"></input>
+</div>
+</body>
+</html>
